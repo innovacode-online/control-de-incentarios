affaabb22de270eee08cecd775e04d35e1d60a12
Ale <duartemattias4@gmail.com>
2024-01-16 14:57:25 -0400

    sales migrations

---
+++ database/migrations/2024_01_16_170318_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('client');
+            $table->decimal('total');
+            $table->foreignId('user_id')
+                ->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropForeign('sales_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+        Schema::dropIfExists('sales');
+
+    }
+};
